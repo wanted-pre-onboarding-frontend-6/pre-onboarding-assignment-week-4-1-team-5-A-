668181bcedb2e64de37a2783974061aabb16ab52
leejha <jhl2619@naver.com>
2022-09-23 23:53:29 +0900

    feat: login api 추가

---
+++ src/apis/user/UserApi.ts
@@ -0,0 +1,54 @@
+import userStorage from '../../utils/userStorage';
+import Http from '../coreApi';
+
+interface ParamsType {
+  params?: string | object;
+  data?: any;
+  userId?: number | undefined;
+}
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+class UserApi {
+  http: any;
+  path: string;
+  accessToken: string | null;
+
+  constructor() {
+    this.accessToken = userStorage.get();
+    this.http = new Http(
+      process.env.REACT_APP_API_URL,
+      this.accessToken ? this.accessToken : undefined,
+    );
+    this.path = '/users';
+  }
+
+  // public addUser({ data }: ParamsType) {
+  //   return this.http.post(this.path, data);
+  // }
+
+  // public getList({ params }: ParamsType) {
+  //   return this.http.get(this.path, params);
+  // }
+
+  // public getListALL() {
+  //   return this.http.get(this.path);
+  // }
+
+  // public getInfo({ params }: ParamsType) {
+  //   return this.http.get(this.path, params);
+  // }
+
+  public login(data: LoginRequest) {
+    console.log(data);
+    return this.http.post('/login', data);
+  }
+
+  // public getUserSetting({ params }: ParamsType) {
+  //   return this.http.get('/userSetting', params);
+  // }
+}
+export default new UserApi();
